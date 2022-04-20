f31b2a27dcaff539c3f5231eed917dc361aaf769
segundo vallejos <segundovallejos@gmail.com>
2022-04-20 11:00:48 -0300

    terera entrega

--- app_vallejos/src/components/jsx/NavBar.js
+++ app_vallejos/src/components/jsx/NavBar.js
@@ -13,7 +13,7 @@ const NavBar =(props)=>{
                     <li><a href="#">{props.link[1]}</a></li>
                     <li><a href="#">{props.link[2]}</a></li>
                     <li><a href="#">{props.link[3]}</a> </li>
-                    <li><a href="#"><span class="material-icons">shopping_cart</span></a></li>
+                    <li><a href="#"><span className="material-icons">shopping_cart</span></a></li>
                 </ul>         
             </nav>
         )
